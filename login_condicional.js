964ec5c1ada04541dd367a2c78eb04c318e323b0
gcancho <giovannicb19@gmail.com>
2021-01-21 12:49:45 -0500

    ejercicio en react con extension js

--- login_condicional.js
+++ login_condicional.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const usuario = 'Giovanni';
-const sesion = false;
+const sesion = true;
+const pais = 'Perú';
 
 const microfono =(
   <>
-    {sesion===true ? 
+    {sesion === true ? 
     <>
     <h1>Bienvenido {usuario}</h1>
     <p>Que tengas un buen día</p>
+
+    {/* Si el pais existe entonces mostrar el párrafo, de modo contrario no mostrarlo */}
+    {pais && <p>Tu eres del país de : {pais}</p>}
     </>
     : <p>No ha podido iniciar sesion</p>}
   </>
