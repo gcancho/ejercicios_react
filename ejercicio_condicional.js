f18de74c1ea3ee3f409025ce0f49edcdc744b4ef
gcancho <giovannicb19@gmail.com>
2021-01-21 13:36:55 -0500

    ejercicio en react con extension js edit

--- ejercicio_condicional.js
+++ ejercicio_condicional.js
@@ -4,16 +4,25 @@ import ReactDOM from 'react-dom';
 const usuario = 'Giovanni';
 const sesion = true;
 const pais = 'Perú';
+const amigos = ['carlos','marcos','jorge'];
 
 const microfono =(
   <>
-    {sesion === true ? 
+    {sesion===true ? 
     <>
     <h1>Bienvenido {usuario}</h1>
     <p>Que tengas un buen día</p>
 
     {/* Si el pais existe entonces mostrar el párrafo, de modo contrario no mostrarlo */}
     {pais && <p>Tu eres del país de : {pais}</p>}
+
+    <p>Mis amigos son :</p>
+    <ul>
+      {/* Con map puedo recorrer el arreglo creado, con react puedo retornar codigo jsx y agregarle key para evitar error en consola*/}
+      {amigos && amigos.map((amix,index)=>{
+        return <li key={index}>{amix}</li>
+      })}
+    </ul>
     </>
     : <p>No ha podido iniciar sesion</p>}
   </>
